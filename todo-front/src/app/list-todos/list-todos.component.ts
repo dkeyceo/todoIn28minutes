773549f4877cb3c8181470fd7a71b6bda18a1d69
dkeyceo <dkeyceo@gmail.com>
2023-07-11 22:18:32 +0300

    Added Todo class as a model for todo

--- todo-front/src/app/list-todos/list-todos.component.ts
+++ todo-front/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public desc: string,
+    public done: boolean,
+    public targetDate: Date
+  ){}
+}
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,18 +16,14 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos = [
-    {
-      id: 1,
-      desc: 'Learn to dance'
-    },
-    {
-      id: 2,
-      desc: 'buy milk'
-    },
-    {
-      id: 3,
-      desc: 'Earn 1 billion'
-    }
+    /*
+    { id: 1, desc: 'Learn to dance'},
+    { id: 2, desc: 'buy milk'},
+    { id: 3, desc: 'Earn 1 billion $'}
+    */
+   new Todo(1, 'Learn to dance' , false, new Date()),
+   new Todo(2, 'Buy milk' , false, new Date()),
+   new Todo(3, 'Earn 1 billion $' , false, new Date()),
   ];
 
   todo = {
