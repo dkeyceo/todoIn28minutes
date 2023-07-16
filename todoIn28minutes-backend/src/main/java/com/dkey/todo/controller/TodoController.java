ad4c4eb9b029d31244faf48460ace4b6120838cc
dkeyceo <dkeyceo@gmail.com>
2023-07-16 10:21:56 +0300

    Added delete method to backend todo service and controller

--- todoIn28minutes-backend/src/main/java/com/dkey/todo/controller/TodoController.java
+++ todoIn28minutes-backend/src/main/java/com/dkey/todo/controller/TodoController.java
@@ -3,7 +3,9 @@ package com.dkey.todo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +22,15 @@ public class TodoController {
 	@GetMapping("/users/{username}/todos")
 	public List<Todo> getAllTodos(@PathVariable("username") String username){
 		return todoHardcodedService.findAll();
+	}
+	
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity deleteTodo(@PathVariable String username, @PathVariable long id) {
+		Todo todo = todoHardcodedService.deleteById(id);
+		
+		if(todo != null) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.notFound().build();
 	}
 }
