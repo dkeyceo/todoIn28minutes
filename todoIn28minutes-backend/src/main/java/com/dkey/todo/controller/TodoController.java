d523166a5a637916d3034f08cbb2e24a9205450b
dkeyceo <dkeyceo@gmail.com>
2023-07-17 06:50:54 +0300

    Added create todo logic in service and controller for backend

--- todoIn28minutes-backend/src/main/java/com/dkey/todo/controller/TodoController.java
+++ todoIn28minutes-backend/src/main/java/com/dkey/todo/controller/TodoController.java
@@ -1,5 +1,6 @@
 package com.dkey.todo.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dkey.todo.model.Todo;
 import com.dkey.todo.service.TodoHardcodedService;
@@ -20,7 +23,7 @@ import com.dkey.todo.service.TodoHardcodedService;
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class TodoController {
-	@Autowired
+	@Autowired 
 	private TodoHardcodedService todoHardcodedService;
 	
 	@GetMapping("/users/{username}/todos")
@@ -33,12 +36,23 @@ public class TodoController {
 		return todoHardcodedService.findById(id);
 	}
 	
+	@PostMapping("/users/{username}/todos")
+	public ResponseEntity<?> createTodo(@PathVariable("username") String username, 
+			@RequestBody Todo todo){
+		Todo todoCreated = todoHardcodedService.save(todo);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(todoCreated.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
 	@PutMapping("/users/{username}/todos/{id}")
 	public ResponseEntity<Todo> updateTodo(@PathVariable("username") String username, 
 			@PathVariable long id,
 			@RequestBody Todo todo){
 		Todo todoUpdated = todoHardcodedService.save(todo);
-		return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+		return new ResponseEntity<Todo>(todoUpdated, HttpStatus.OK);
 	}
 	
 	@DeleteMapping("/users/{username}/todos/{id}")
