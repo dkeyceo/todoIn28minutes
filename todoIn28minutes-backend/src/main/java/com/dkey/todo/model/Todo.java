af37abd01c7f94339815c34186099c3220f5077e
dkeyceo <dkeyceo@gmail.com>
2023-07-21 07:09:29 +0300

    Added Enity annotation, and used h2 in mem and jpa

--- todoIn28minutes-backend/src/main/java/com/dkey/todo/model/Todo.java
+++ todoIn28minutes-backend/src/main/java/com/dkey/todo/model/Todo.java
@@ -2,7 +2,15 @@ package com.dkey.todo.model;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Todo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String username;
 	private String description;
